a4eb9e782700b6df261380e0a86bee561345e914
Areeba Rafiq <140241495+Areeba000@users.noreply.github.com>
2023-10-25 13:35:14 +0500

    Update ERC20.sol

--- ERC20contract/ERC20.sol
+++ ERC20contract/ERC20.sol
@@ -1,60 +1,81 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.20;
 
 interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address _owner) external view returns (uint256);
-   function allowance(address _owner, address _spender) external view returns (uint256);
-
-    function transfer(address _to, uint256 _value) external returns (bool);
-   function approve(address _spender, uint256 _value) external returns (bool);
-   function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
-
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
-contract MYERC20token is IERC20 {
-    mapping (address => uint256)public _balance;
-    mapping(address=>mapping(address =>uint256))public _allowed;
-    string public name="abeertraining";
-    string public symbol ="cat";
-    uint public decimals=0;
-    uint256 private _totalsupply;
-    address public _creator;
-    constructor () public{
-        _creator=msg.sender;
-        _totalsupply=50000;
-        _balance[_creator]=_totalsupply;
-          
+
+
+contract ERC20 is IERC20{
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name ;
+    string public symbol;
+    uint8 public decimals ;
+    address public owner;
+    constructor(string memory _name,string memory _symbol,uint8 _decimals){
+        owner=msg.sender;
+        name=_name;
+        symbol=_symbol;
+        decimals=_decimals;
+
     }
-    function totalSupply() external view returns (uint256){
-        return _totalsupply;
+    modifier  onlyowner{
+        require(owner==msg.sender,"you are not owner");
+        _;
     }
-    function balanceOf(address _owner) external view returns (uint256){
-        return _balance[_owner];
+
+    function transfer(address recipient, uint amount) external returns(bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
     }
-    function transfer(address _to, uint256 _value) external returns (bool){
-       require(_value>0 && _balance[msg.sender]>=_value);
-        _balance [_to]+=_value;
-        _balance[msg.sender]-=_value;
-    
-    emit Transfer(msg.sender,  _to,_value);
-return true;
+
+    function approve(address spender, uint amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
-    function approve(address _spender, uint256 _value)  public returns (bool){
-require(_value>0 && _balance[msg.sender]>=_value);
-_allowed[msg.sender][_spender]=_value;
- emit Approval(msg.sender,  _spender, _value);
- return true;
+
+    function transferFrom(address sender,address recipient,  uint amount ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(address account,uint256 amount) external onlyowner {
+          require(account != address(0), "ERC20: mint to the zero address");
+        totalSupply += amount;
+       balanceOf[account] += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(address account,uint256 amount) external onlyowner {
+        require(account != address(0), "ERC20: mint to the zero address");
+         balanceOf[account] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+    function allowancefun(address _owner, address _spender) external view returns (uint256){
+    return  allowance[_spender][_owner];
+    } 
+      function gettotalSupply() external view returns (uint256){
+        return totalSupply;
+    }
+    function getbalanceOf(address _owner) external view returns (uint256){
+        return balanceOf[_owner];
+
+    }
 }
-function transferFrom(address _from, address _to, uint256 _value) external returns (bool){
-     require(_value>0 && _balance[_from]>=_value &&  _allowed[_from][_to]>=_value);
-        _balance [_to]+=_value;
-        _balance[_from]-=_value;
-    _allowed[_from][_to]=_value;
-return true;
-}
-function allowance(address _owner, address _spender) external view returns (uint256){
-return _allowed[_spender][_owner];
-}  
-  }
